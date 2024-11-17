c0e7c2f501408aade610b247ae4f3e2bda3c3fb5
AdrishikharChowdhury <amiadrishikhar@gmail.com>
2024-11-17 20:51:20 +0530

    Added 2 player system

--- Mini projects/Guess the number.py
+++ Mini projects/Guess the number.py
@@ -1,15 +1,15 @@
 import random
-def guesser(x,target):
+def guesser(name,x,target):
     if x=="Quit":
        return False
     x=int(x)
     if x==target:
-        print("You Won.You have guessed the correct number:\nAns: ",target)
+        print(name," won.You have guessed the correct number:","\nAns",target)
         return False
     elif x<target:
-        print("You have entered a small number.Guess bigger number: ")
+        print(name," you have entered a small number.Guess bigger number: ")
     else:
-        print("You have guessed big number.Guess smaller number: ")
+        print(name," you have guessed big number.Guess smaller number: ")
 
 def difficulty():
     print("Enter the game difficulty:")
@@ -37,13 +37,44 @@ def difficulty():
                 print("Wrong Input!!!!Try Again")
 n1,n2,turns=difficulty()
 target=random.randint(n1,n2)
-while turns!=0:
-    x=input("Enter the number to guess or Quit: ")
-    ip=guesser(x,target)
-    if ip==False:
-        break
-    turns-=1
-    print("Turn Left: ",turns)
-if(turns==0):
-    print("You lost.The number was:",target)
+print("Choose your game mode:")
+print("1.Single player\n2.Two player")
+j=True
+while j:
+    j=False
+    ch=int(input("Enter your choice: "))
+    match ch:
+        case 1:
+            name1=input("Enter your name player: ")
+            while turns!=0:
+                x=input("Enter the number to guess or Quit: ")
+                ip=guesser(name1,x,target)
+                if ip==False:
+                    break
+                turns-=1
+                print("Turn Left: ",turns)
+            if(turns==0):
+                print("You lost.The number was:",target)
+        case 2:
+            name1=input("Enter your name player 1: ")
+            name2=input("Enter your name player 2: ")
+            while turns!=0:
+                x1=int(input(f"{name1} Enter the number to guess or Quit: "))
+                x2=int(input(f"{name2} Enter the number to guess or Quit: "))
+                ip1=guesser(name1,x1,target)
+                ip2=guesser(name2,x2,target)
+                if ip1==False:
+                    print(name2," lost.Better Luck Next Time")
+                    break
+                if ip2==False:
+                    print(name1," lost.Better luck next time")
+                    break
+                turns-=1
+                print("Turn Left: ",turns)
+            if(turns==0):
+                print("Both of you lost.The number was:",target)
+                print("Better Luck Next Time")
+        case _:
+            print("Sorry it is not available for multiplayer (Coming Soon...).")
+            j=True
 print("=====GAME OVER=====")
