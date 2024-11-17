d1785b513fd3cf990d919747c01f4f5e24eab2ef
AdrishikharChowdhury <amiadrishikhar@gmail.com>
2024-11-17 13:27:39 +0530

    Added turns

--- Mini projects/Guess the number.py
+++ Mini projects/Guess the number.py
@@ -1,4 +1,16 @@
 import random
+def guesser(x,target):
+    if x=="Quit":
+       return False
+    x=int(x)
+    if x==target:
+        print("You Won.You have guessed the correct number:\nAns: ",target)
+        return False
+    elif x<target:
+        print("You have entered a small number.Guess bigger number: ")
+    else:
+        print("You have guessed big number.Guess smaller number: ")
+
 def difficulty():
     print("Enter the game difficulty:")
     print("1.Easy\n2.Medium\n3.Hard\n4.Nightmare")
@@ -7,31 +19,31 @@ def difficulty():
         match(ch):
             case 1:
                 print("Chosen Difficulty: Easy")
-                return 1,50
+                print("No. of turns: ",15)
+                return 1,50,14
             case 2:
                 print("Chosen Difficulty: Medium")
-                return 1,100
+                print("No. of turns: ",12)
+                return 1,100,12
             case 3:
                 print("Chosen Difficulty: Hard")
-                return 1,500
+                print("No. of turns: ",10)
+                return 1,500,10
             case 4:
                 print("Chosen Difficulty: Nightmare")
-                return 1,1000
+                print("No. of turns: ",8)
+                return 1,1000,8
             case _:
                 print("Wrong Input!!!!Try Again")
-
-n1,n2=difficulty()
+n1,n2,turns=difficulty()
 target=random.randint(n1,n2)
-while True:
+while turns!=0:
     x=input("Enter the number to guess or Quit: ")
-    if x=="Quit":
-       break
-    x=int(x)
-    if x==target:
-        print("You have guessed the correct number:\nAns: ",target)
+    ip=guesser(x,target)
+    if ip==False:
         break
-    elif x<target:
-        print("You have entered a small number.Guess bigger number: ")
-    else:
-        print("You have guessed big number.Guess smaller number: ")
+    turns-=1
+    print("Turn Left: ",turns)
+if(turns==0):
+    print("You lost.The number was:",target)
 print("=====GAME OVER=====")
